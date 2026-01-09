8ba2f13b885ac3c34051d98f18e6a10c50ce533c
TrangLe265 <trang.le@myy.haaga-helia.fi>
2026-01-09 15:55:58 +0700

    enable soft-deletet and fix post invoice logic

--- app/routers/invoices.py
+++ app/routers/invoices.py
@@ -40,33 +40,29 @@ def get_invoice(invoice_id, db: Session = Depends(get_db)):
 @router.post("/", response_model=schemas.InvoiceResponse)
 def create_invoice(invoice: schemas.InvoiceCreate, db: Session = Depends(get_db)):
     #check if client_id and gig_id exist
-    if (db.query(models.Client).filter(models.Client.id == invoice.client_id).first() is None):
+    if db.query(models.Client).filter(models.Client.id == invoice.client_id).first() is None:
         raise HTTPException(status_code=400, detail="Client with the given id does not exist")
-    if (db.query(models.Gig).filter(models.Gig.id == invoice.gig_id).first() is None):
+    if db.query(models.Gig).filter(models.Gig.id == invoice.gig_id).first() is None:
         raise HTTPException(status_code=400, detail="Gig with the given id does not exist")
+    
+    #check if gig_id already has an invoice
+    if db.query(models.Invoice).filter(models.Invoice.gig_id == invoice.gig_id).first() is not None:
+        raise HTTPException(status_code=400, detail="An invoice for the given gig_id already exists")
 
+    #check if gig's sttatus is not 'cancelled'
+    db_gig = db.query(models.Gig).filter(models.Gig.id == invoice.gig_id).first()
+    if db_gig.status == schemas.GigStatus.cancelled:
+        raise HTTPException(status_code=400, detail="Cannot create invoice for a cancelled gig")
+        
     #check if the gig belongs to the client
-    if (db.query(models.Gig).filter(models.Gig.client_id != invoice.client_id).first()):
-        raise HTTPException(status_code=400, detail="Gig does not belong to the specified client")
+    if db_gig.client_id != invoice.client_id:
+        raise HTTPException(status_code=400, detail="The gig does not belong to the specified client")
 
-    db_invoice = models.Invoice(**invoice.dict())
-    db.add(db_invoice)
+    new_invoice = models.Invoice(**invoice.dict())
+    db.add(new_invoice)
     db.commit()
-    db.refresh(db_invoice)
-    return db_invoice
-    if db_invoice is None:
-        raise HTTPException(status_code=400, detail="Invoice could not be created")
-
-
-@router.delete("/{invoice_id}", response_model=schemas.InvoiceResponse)
-def delete_invoice(invoice_id: int, db: Session = Depends(get_db)):
-    db_invoice = db.query(models.Invoice).filter(models.Invoice.id == invoice_id).first()
-    if not db_invoice:
-        raise HTTPException(status_code=404, detail="Invoice not found")
-    db.delete(db_invoice)
-    db.commit()
-    return db_invoice
-
+    db.refresh(new_invoice)
+    return new_invoice
 
 @router.put("/{invoice_id}", response_model=schemas.InvoiceResponse)
 def update_invoice(invoice_id: int, invoice: schemas.InvoiceUpdate, db: Session = Depends(get_db)):
